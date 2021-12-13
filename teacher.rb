3794441566859e1fbfe3133e901d6fce6ce62652
mmsesay <maej_01@hotmail.com>
2021-12-13 19:10:48 +0000

    Add teacher class

---
+++ teacher.rb
@@ -0,0 +1,11 @@
+require './person'
+
+class Teacher < Person
+  def initialize(age, specialization, name = 'Unknown', parent_permission = true)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
