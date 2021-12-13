037ec060d7d9f1bce8381f26ef398d640435a56d
mmsesay <maej_01@hotmail.com>
2021-12-13 22:52:18 +0000

    Update readme.md file

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 class Person
   attr_accessor :id, :name, :age
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1.100)
     @name = name
     @age = age
@@ -9,12 +9,12 @@ class Person
   end
 
   def can_use_services?
-    is_of_age || @parent_permission
+    of_age || @parent_permission
   end
 
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 end
